2c64ad34955676d0ecb057194c72427368d0e5b7
Franklin-F <dewujie64@gmail.com>
2024-05-09 14:34:27 +0800

    新增header 功能完成

--- Reptile_Tools/ViewModels/MVVMHeaderToolsPageViewModel.cs
+++ Reptile_Tools/ViewModels/MVVMHeaderToolsPageViewModel.cs
@@ -3,7 +3,9 @@ using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -31,23 +33,37 @@ namespace Reptile_Tools.ViewModels
                 Dictstring = "";
                 return;
             }
-            var headers = Headerstring.Split('\n');
             var dict = new Dictionary<string, string>();
-            foreach (var header in headers)
+            Regex regex = new Regex(@"^([\w-]+):\s*(.+)$", RegexOptions.Multiline);
+            MatchCollection matches = regex.Matches(Headerstring);
+            foreach (Match match in matches)
             {
-                var headerkv = header.Split(':');
-                if (headerkv.Length == 2)
+                dict.Add(match.Groups[1].Value, match.Groups[2].Value.Replace("\r",""));
+            }
+            
+            if (dict.Count < 1)
+            {
+                Dictstring = "";
+                return;
+            }
+            else
+            { 
+                string variable = "headers = {\r\n\t";
+                int flag = 0;
+                foreach (KeyValuePair<string, string> i in dict)
                 {
-                    dict.Add(headerkv[0].Trim(), headerkv[1].Trim());
+                    string a = "\"";
+                    if (flag != 0)
+                    {
+                        a = ",\r\n\t\"";
+                    }
+                    variable += a + i.Key.Trim() + "\"" + ": \"" + i.Value.Trim().Replace("\"","\\\"") + "\"";
+                    flag++;
                 }
+                variable += "\r\n}\r\n";
+                Dictstring = variable;
             }
-            Dictstring = "{\n";
-            foreach (var kv in dict)
-            {
-                Dictstring += $"\"{kv.Key}\":\"{kv.Value}\",\n";
-            }
-            Dictstring = Dictstring.TrimEnd(',', '\n');
-            Dictstring += "\n}";
+            
         }
     }
 }
