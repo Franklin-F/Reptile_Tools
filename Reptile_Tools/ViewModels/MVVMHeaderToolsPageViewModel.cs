cbc55627cb53ed80be14699a66665184840bcf5b
Franklin-F <dewujie64@gmail.com>
2024-05-09 12:17:53 +0800

    新增header 页面 功能等下修改

---
+++ Reptile_Tools/ViewModels/MVVMHeaderToolsPageViewModel.cs
@@ -0,0 +1,53 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Reptile_Tools.ViewModels
+{
+    partial class MVVMHeaderToolsPageViewModel: ObservableObject
+    {
+        [ObservableProperty]
+        public double _textboxmaxheight;
+        [ObservableProperty]
+        public string? _headerstring;
+        [ObservableProperty]
+        public string? _dictstring;
+        [RelayCommand]
+        public void Cleared()
+        {
+            Headerstring = "";
+            Dictstring = "";
+        }
+        [RelayCommand]
+        public void UpdaterDict()
+        {
+            if (Headerstring == null || Headerstring.Trim() == "")
+            {
+                Dictstring = "";
+                return;
+            }
+            var headers = Headerstring.Split('\n');
+            var dict = new Dictionary<string, string>();
+            foreach (var header in headers)
+            {
+                var headerkv = header.Split(':');
+                if (headerkv.Length == 2)
+                {
+                    dict.Add(headerkv[0].Trim(), headerkv[1].Trim());
+                }
+            }
+            Dictstring = "{\n";
+            foreach (var kv in dict)
+            {
+                Dictstring += $"\"{kv.Key}\":\"{kv.Value}\",\n";
+            }
+            Dictstring = Dictstring.TrimEnd(',', '\n');
+            Dictstring += "\n}";
+        }
+    }
+}
---
+++ Reptile_Tools/Views/Pages/HeaderToolsPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Reptile_Tools.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Reptile_Tools.Views.Pages
+{
+    /// <summary>
+    /// HeaderToolsPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class HeaderToolsPage : Page
+    {
+        public HeaderToolsPage()
+        {
+            this.DataContext = new MVVMHeaderToolsPageViewModel();
+            InitializeComponent();
+        }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var viewmodels = DataContext as MVVMHeaderToolsPageViewModel;
+            if (viewmodels != null)
+            {
+                viewmodels.Textboxmaxheight = (ActualHeight - 48) / 19 * 9;
+            }
+        }
+    }
+}
